be76078fd819b742611e8f3f2dc5a6d17d5ddf81
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-04 00:23:02 +0430

    add provider file

---
+++ src/CartProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mohammadv184\Cart;
+
+use Illuminate\Support\ServiceProvider;
+
+class CartProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        $this->app->bind("cart",function (){
+            return new CartService();
+        });
+
+        $this->mergeConfigFrom(__DIR__."Config/cart.php","cart");
+    }
+
+    public function boot(){
+        $this->publishes([
+            __DIR__."Config/cart.php"=>config_path("cart.php")
+        ],"config");
+
+    }
+
+}
