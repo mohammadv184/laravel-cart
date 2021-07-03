4ce5c0093de3cae87dc641a780b81b73254e07b5
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-04 00:23:33 +0430

    add facade file

---
+++ src/Facades/Cart.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Mohammadv184\Cart\Facades;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class Cart extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return "cart";
+    }
+}
