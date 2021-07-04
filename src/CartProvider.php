72f8152396ee979e3c32eb773230f564f529bfad
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-04 17:03:20 +0430

    refactor CartProvider file

--- src/CartProvider.php
+++ src/CartProvider.php
@@ -9,11 +9,11 @@ class CartProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->bind("cart",function (){
-            return new CartService();
-        });
-
         $this->mergeConfigFrom(__DIR__."/Config/cart.php","cart");
+
+        $this->app->singleton("cart",function (){
+            return new CartService(config("instanceName","cart"));
+        });
     }
 
     public function boot(){
