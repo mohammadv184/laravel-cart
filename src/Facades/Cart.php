1445d4e9de2fa66d2ade1a3d1283adcf30384e96
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-04 17:20:12 +0430

    refactor CartFacade

--- src/Facades/Cart.php
+++ src/Facades/Cart.php
@@ -4,8 +4,24 @@
 namespace Mohammadv184\Cart\Facades;
 
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
+use Mohammadv184\Cart\CartService;
 
+/**
+ * Class Cart
+ * @method static CartService put(array $value,$model=null)
+ * @method static string getInstanceName()
+ * @method static CartService update($value,$key)
+ * @method static CartService has($key)
+ * @method static CartService delete($key)
+ * @method static CartService flush()
+ * @method static array get($id,bool $withRelationShip=true)
+ * @method static Collection all(bool $withRelationShip=true)
+ * @method static int totalPrice()
+ *
+ * @see \Mohammadv184\Cart\CartService
+ */
 class Cart extends Facade
 {
     protected static function getFacadeAccessor()
