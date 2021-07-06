f88358ed73861ed827c2eb653c7a41c3c480014e
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-06 18:53:26 +0430

    update tests

--- tests/CartServiceTest.php
+++ tests/CartServiceTest.php
@@ -13,9 +13,15 @@ class CartServiceTest extends TestCase
      */
     protected $cart;
 
+    /**
+     * @var ModelMock
+     */
     protected $model;
 
 
+    /**
+     *setup test method
+     */
     protected function setUp(): void
     {
 
@@ -23,6 +29,10 @@ class CartServiceTest extends TestCase
         $this->model=new ModelMock();
 
     }
+
+    /**
+     *test Cart put method
+     */
     public function testPut()
     {
         $this->cart->put([
@@ -42,6 +52,10 @@ class CartServiceTest extends TestCase
         $this->assertTrue($this->cart->has("test"));
         $this->assertTrue($this->cart->has($this->model));
     }
+
+    /**
+     *test Cart Instance method
+     */
     public function testInstance(){
         $this->cart->instance("test");
         $this->assertEquals("test",$this->cart->getInstanceName());
@@ -54,6 +68,10 @@ class CartServiceTest extends TestCase
 
 
     }
+
+    /**
+     *test Cart Update method
+     */
     public function testUpdate(){
         $this->cart->put([
             "id"=>"test",
@@ -68,6 +86,194 @@ class CartServiceTest extends TestCase
             'cartable_id' => 1,
             'cartable_type' => 'ModelMock'
         ],$this->cart->get($this->model,false));
+
+        $this->cart->update([
+            "id"=>"test2",
+            "quantity"=>960,
+            "price"=>0
+        ],$this->model);
+
+        $this->assertEquals([
+            "id"=>"test2",
+            "quantity"=>960,
+            "price"=>0,
+            'cartable_id' => 1,
+            'cartable_type' => 'ModelMock'
+        ],$this->cart->get($this->model,false));
+
+    }
+
+    /**
+     *test Cart Has method
+     */
+    public function testHas(){
+        $this->cart->put([
+            "id"=>"test",
+            "quantity"=>10000,
+            "price"=>1000
+        ],$this->model);
+        $this->assertTrue($this->cart->has($this->model));
+
+
+        $this->assertTrue($this->cart->has("test"));
+
+        $this->assertFalse($this->cart->has("test2"));
+
+        $this->model->id=2;
+        $this->assertFalse($this->cart->has($this->model));
+
+    }
+
+    /**
+     *test Cart Delete method
+     */
+    public function testDelete(){
+        $this->cart->put([
+            "id"=>"test",
+            "quantity"=>10000,
+            "price"=>1000
+        ],$this->model);
+
+        $this->assertEquals(1,$this->cart->all(false)->count());
+
+        $this->cart->delete("test");
+
+        $this->assertEquals(0,$this->cart->all(false)->count());
+
+    }
+
+    /**
+     *test Cart Flush method
+     */
+    public function testFlush(){
+        $this->cart->put([
+            "id"=>"test1",
+            "quantity"=>10000,
+            "price"=>1000
+        ],$this->model);
+        $this->model->id=2;
+        $this->cart->put([
+            "id"=>"test2",
+            "quantity"=>9508,
+            "price"=>384
+        ],$this->model);
+        $this->model->id=3;
+        $this->cart->put([
+            "id"=>"test3",
+            "quantity"=>50,
+            "price"=>1
+        ],$this->model);
+
+        $this->assertEquals(3,$this->cart->all()->count());
+
+        $this->cart->flush();
+
+        $this->assertEquals(0,$this->cart->all()->count());
+
+    }
+
+    /**
+     *test Cart Get method
+     */
+    public function testGet(){
+        $this->cart->put([
+            "id"=>"test1",
+            "quantity"=>10000,
+            "price"=>1000
+        ],$this->model);
+        $this->model->id=2;
+        $this->cart->put([
+            "id"=>"test2",
+            "quantity"=>9508,
+            "price"=>384
+        ],$this->model);
+        $this->model->id=3;
+        $this->cart->put([
+            "id"=>"test3",
+            "quantity"=>50,
+            "price"=>1
+        ],$this->model);
+
+        $this->assertEquals([
+            "id"=>"test3",
+            "quantity"=>50,
+            "price"=>1,
+            'cartable_id' => 3,
+            'cartable_type' => 'ModelMock'
+        ],$this->cart->get("test3",false));
+        $this->model->id=2;
+        $this->assertEquals([
+            "id"=>"test2",
+            "quantity"=>9508,
+            "price"=>384,
+            'cartable_id' => 2,
+            'cartable_type' => 'ModelMock',
+            "modelmock"=>$this->model
+
+        ],$this->cart->get("test2"));
+
+
+    }
+
+    /**
+     *test Cart All method
+     */
+    public function testAll(){
+        $this->cart->put([
+            "id"=>"test1",
+            "quantity"=>10000,
+            "price"=>1000
+        ],$this->model);
+        $this->model->id=2;
+        $this->cart->put([
+            "id"=>"test2",
+            "quantity"=>9508,
+            "price"=>384
+        ],$this->model);
+        $this->model->id=3;
+        $this->cart->put([
+            "id"=>"test3",
+            "quantity"=>50,
+            "price"=>1
+        ],$this->model);
+
+        $this->assertEquals(3,$this->cart->all()->count());
+
+        $this->assertEquals([
+            'id' => 'test3',
+            'price' => 1,
+            'quantity' => 50,
+            'cartable_id' => 3,
+            'cartable_type' => 'ModelMock',
+            'modelmock'=>$this->model
+
+        ],$this->cart->all()["test3"]);
+
+    }
+
+    /**
+     *test Cart TotalPrice method
+     */
+    public function testTotalPrice(){
+        $this->cart->put([
+            "id"=>"test1",
+            "quantity"=>10000,
+            "price"=>1000
+        ],$this->model);
+        $this->model->id=2;
+        $this->cart->put([
+            "id"=>"test2",
+            "quantity"=>9508,
+            "price"=>215
+        ],$this->model);
+        $this->model->id=3;
+        $this->cart->put([
+            "id"=>"test3",
+            "quantity"=>50,
+            "price"=>8000
+        ],$this->model);
+
+        $this->assertEquals(12444220,$this->cart->totalPrice());
 
     }
 }
