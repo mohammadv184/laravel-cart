b99dabddf232cfa0f2a475b35427602e9ebd9011
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-09 15:13:53 +0430

    add migration

---
+++ src/Database/Migrations/0000_00_00_000000_create_Cart_Items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CartItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+                $table->id();
+                $table->string("rowId");
+                $table->unsignedBigInteger("price");
+                $table->integer("quantity");
+                $table->integer("cartable_id");
+                $table->string("cartable_type");
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
