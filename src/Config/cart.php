1d4bf4402fb1c2ff80b3f65b9360dab16b404800
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-11 23:32:12 +0430

    add config phpdoc

--- src/Config/cart.php
+++ src/Config/cart.php
@@ -1,6 +1,25 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | laravel cart session key
+    |--------------------------------------------------------------------------
+    |
+    | This default instanceName will be used when you put a Cart item
+    | used in session key.
+    |
+    */
     'instanceName'=> 'cart',
+
+    /*
+   |--------------------------------------------------------------------------
+   | laravel cart table name
+   |--------------------------------------------------------------------------
+   |
+   | Here you can set the table name that the laravel cart should use in
+   | migration and database table name.
+   |
+   */
     'table'       => 'cart_items',
 ];
